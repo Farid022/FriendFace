3a0d96bd38fd000c4c8a72c5f697f6df1999c66f
Muhammad Farid Ullah <faridkhan125janan@gmail.com>
2023-01-04 05:23:30 +0500

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Muhammad Farid Ullah on 04/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
